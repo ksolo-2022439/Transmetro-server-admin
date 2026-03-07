e24b8db1ec3e6925f67fec268c6e92da29f23362
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2026-03-06 18:32:09 -0600

    feat: Inicializar configuraciones de 	- server-admin 	- app.js 	- cors-configuration.js 	- db.js 	- helmet-configuration.js

---
+++ server-admin/configs/app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+//Importaciones
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { corsOptions } from './cors-configuration.js';
+import { dbConnection } from './db.js'; 
+import { createAdminSeed } from './admin.seed.js'
+
+//Rutas
+//EJEMPLO: import accountRoutes from '../src/accounts/account.routes.js';
+
+const BASE_URL = '/TRANSMETRO-CONECTA/v1';
+
+//Configuración de mi aplicación
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+    app.use(express.json({ limit: '10mb' }));
+    app.use(cors(corsOptions));
+    app.use(morgan('dev'));
+}
+
+//Integracion de todas las rutas
+const routes = (app) => {
+    //EJEMPLO: app.use(`${BASE_URL}/accounts`, accountRoutes);
+}
+
+//FUNCIÓN PARA INICIAR EL SERVIDOR
+const initServer = async () => {
+    //Creación de la instancia de la aplicaccion
+    const app = express();
+    const PORT = process.env.PORT || 3001;
+
+    try {
+        //Conexión a Base de Datos (Esperamos a que conecte)
+        await dbConnection();
+
+        //CONFIGURACIONES DEL MIDDLEWARES Y RUTAS
+        middlewares(app);
+        routes(app);
+        createAdminSeed(app);
+
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en el puerto ${PORT}`);
+            console.log(`Base URL: http://localhost:${PORT}${BASE_URL}`);
+        });
+
+        //Primera ruta (Health Check)
+        app.get(`${BASE_URL}/health`, (req, res) => {
+            res.status(200).json({
+                status: 'ok',
+                service: 'BIK Admin',
+                version: '1.0.0'
+            });
+        });
+
+    } catch (error) {
+        console.log('Error al iniciar el servidor:', error);
+    }
+}
+
+export { initServer };
